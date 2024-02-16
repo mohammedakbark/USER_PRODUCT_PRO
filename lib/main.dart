8f1e4dcb33efd16fcbf5493cbe30f0f2412176fc
mohammedakbark <akbarmohd00123@gmail.com>
2024-02-16 13:52:15 +0530

    firebase and SHA updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:hardware_pro/firebase_options.dart';
 import 'package:hardware_pro/my_order_status.dart';
 import 'package:hardware_pro/claim_warrenty_request_submited.dart';
 import 'package:hardware_pro/sign/lets_go.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,7 +18,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return FlutterSizer(builder: (context,orientation,screentype) {
+    return FlutterSizer(builder: (context, orientation, screentype) {
       return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
